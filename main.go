99278f5b38331d8923b043ff85b64e327951be95
thestuckster <sstucky3@gmail.com>
2024-08-27 21:26:10 -0500

    🔊 add structured logger

--- main.go
+++ main.go
@@ -3,19 +3,28 @@ package main
 import (
 	"errors"
 	"fmt"
+	"github.com/rs/zerolog"
 	"github.com/thestuckster/gopherfacts/pkg/clients"
 	"os"
 	"sync"
 	"time"
 )
 
-func main() {
-	time.Sleep(58 * time.Second)
+var logger = zerolog.New(os.Stdout).With().Timestamp().Caller().Logger()
 
-	fmt.Println("starting gopherfacts")
+func main() {
+
+	logger.Info().Msg("Starting Gopherfacts...")
+
+	wait := 60
+	logger.Debug().Msgf("Waiting for %d seconds for all cooldowns to finish", wait)
+	time.Sleep(time.Duration(wait) * time.Second)
+
 	token := os.Getenv("TOKEN")
 	character := "Billbert"
 	minerCharacter := "AMiner"
+	//crafter := "smellyfeet"
+
 	client := clients.NewClient(&token)
 
 	var wg sync.WaitGroup
@@ -29,9 +38,13 @@ func main() {
 
 func farmChickens(name string, client *clients.GopherFactClient, wg *sync.WaitGroup) {
 	defer wg.Done()
-	fmt.Println("farming chickens")
+
+	chickenLogger := logger.With().Str("method", "farmChicksn").Str("character", name).Logger()
+
+	chickenLogger.Info().Msg("Farming Chickens")
 	_, err := client.EasyClient.MoveToChickens(name)
 	if err != nil {
+		chickenLogger.Error().Err(err).Msg("Farming Chickens failed")
 		panic(err)
 	}
 
@@ -41,22 +54,23 @@ func farmChickens(name string, client *clients.GopherFactClient, wg *sync.WaitGr
 		if err != nil {
 			var ex *clients.CharacterInventoryFullException
 			if errors.As(err, &ex) {
-				fmt.Println("%%%%%")
-				fmt.Println(ex.Message)
+				chickenLogger.Debug().Msg("Inventory full")
 				dumpInventoryIntoBank(name, client)
 				_, err := client.EasyClient.MoveToChickens(name)
 				if err != nil {
+					chickenLogger.Error().Err(err).Msg("Moving back to chickens after bank dump failed")
 					panic(err)
 				}
 			} else {
+				chickenLogger.Error().Err(err).Msg("Fighting chickens failed")
 				panic(err)
 			}
 		}
 
 		if err == nil {
-			fmt.Printf("FIGHT: turn %d: Got %d xp from fight\n", turns, fightData.Fight.Xp)
+			chickenLogger.Info().Msgf("turn %d: Got %d xp from fight\n", turns, fightData.Fight.Xp)
 			coolDown := fightData.Cooldown.TotalSeconds
-			fmt.Printf("Fighting done. Cooling down for %d seconds\n", coolDown)
+			chickenLogger.Info().Msgf("Fighting done. Cooling down for %d seconds\n", coolDown)
 			time.Sleep(time.Duration(coolDown) * time.Second)
 			turns++
 		}
@@ -65,23 +79,26 @@ func farmChickens(name string, client *clients.GopherFactClient, wg *sync.WaitGr
 }
 
 func farmCopper(name string, client *clients.GopherFactClient, wg *sync.WaitGroup) {
-	wg.Done()
+	defer wg.Done()
 
-	fmt.Println("farming copper")
+	copperLogger := logger.With().Str("method", "farmChicksn").Str("character", name).Logger()
+
+	copperLogger.Info().Msg("Farming Copper")
 	turns := 0
 	for {
 		gatherData, err := client.EasyClient.MineCopper(name)
 		if err != nil {
 			var ex *clients.CharacterInventoryFullException
 			if errors.As(err, &ex) {
-				fmt.Println("%%%%%")
-				fmt.Println(ex.Message)
+				copperLogger.Debug().Msg("Inventory full")
 				dumpInventoryIntoBank(name, client)
 				_, err := client.EasyClient.MoveToChickens(name)
 				if err != nil {
+					copperLogger.Debug().Err(err).Msg("Moving back to copper after bank dump failed")
 					panic(err)
 				}
 			} else {
+				copperLogger.Debug().Err(err).Msg("Farming Copper failed")
 				panic(err)
 			}
 		}
@@ -92,7 +109,7 @@ func farmCopper(name string, client *clients.GopherFactClient, wg *sync.WaitGrou
 				message += fmt.Sprintf("    %d %s\n", item.Quantity, item.Code)
 			}
 
-			println(message)
+			copperLogger.Info().Msg(message)
 			turns++
 		}
 	}
@@ -100,17 +117,21 @@ func farmCopper(name string, client *clients.GopherFactClient, wg *sync.WaitGrou
 
 func dumpInventoryIntoBank(name string, client *clients.GopherFactClient) {
 
+	dumpLogger := logger.With().Str("method", "dumpInventoryIntoBank").Str("character", name).Logger()
+
 	charData, err := client.CharacterClient.GetCharacterInfo(name)
 	if err != nil {
+		dumpLogger.Error().Err(err).Msg("Character info failed")
 		panic(err)
 	}
 
 	inventory := charData.Inventory
 	for _, item := range inventory {
-		fmt.Printf("Dumping all %s into bank\n", item.Code)
+		dumpLogger.Info().Msgf("Dumping all %s into bank\n", item.Code)
 		if item.Code != "" {
 			_, err := client.EasyClient.DepositIntoBank(name, item.Code, item.Quantity)
 			if err != nil {
+				dumpLogger.Error().Err(err).Msg("Deposit into bank failed")
 				panic(err)
 			}
 		}
