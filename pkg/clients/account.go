8bcf5b7b71eba96fb63df82d9626fba4f8f381d7
thestuckster <sstucky3@gmail.com>
2024-09-18 14:00:22 -0500

    :sparkles: implement get all bank items method

--- pkg/clients/account.go
+++ pkg/clients/account.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/thestuckster/gopherfacts/internal"
+	"io"
+	"net/http"
 )
 
 type MyAccountClient struct {
@@ -30,7 +32,7 @@ func (c *MyAccountClient) GetBankGold() (*BankGold, Error) {
 	return &data.Data, nil
 }
 
-type GetBankItemsResponse struct {
+type getBankItemResponse struct {
 	Data  []Item `json:"data"`
 	Total int    `json:"total"`
 	Page  int    `json:"page"`
@@ -38,9 +40,48 @@ type GetBankItemsResponse struct {
 	Pages int    `json:"pages"`
 }
 
-func (c *MyAccountClient) GetBankItems(itemCode *string, page, size int) (*GetBankItemsResponse, Error) {
-	//TODO:
-	return nil, nil
+func (c *MyAccountClient) GetAllBankItems() ([]Item, Error) {
+
+	items := make([]Item, 0)
+	page := 1
+	itemData, err := c.requestBankItems(page)
+	if err != nil {
+		return nil, err
+	}
+
+	items = append(items, itemData.Data...)
+	for page <= itemData.Pages {
+		page++
+		itemData, err = c.requestBankItems(page)
+		if err != nil {
+			return nil, err
+		}
+
+		items = append(items, itemData.Data...)
+	}
+
+	return items, nil
+}
+
+func (c *MyAccountClient) requestBankItems(page int) (*getBankItemResponse, error) {
+	url := fmt.Sprintf("https://api.artifactsmmo.com/my/bank/items?page=%d", page)
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var data getBankItemResponse
+	if err := json.Unmarshal(body, &data); err != nil {
+		return nil, err
+	}
+
+	return &data, nil
 }
 
 type bankExpansionResponse struct {
@@ -51,7 +92,7 @@ type BankExpansionData struct {
 	Cooldown Cooldown `json:"cooldown"`
 }
 
-func (c *CharacterClient) BuyBankExpansion() (*BankExpansionData, Error) {
+func (c *MyAccountClient) BuyBankExpansion() (*BankExpansionData, Error) {
 	req := internal.BuildPostRequestNoBody(BANK_EXPANSION, *c.token)
 	resp, respBody := internal.MakeHttpRequest(req, false)
 	if resp.StatusCode != 200 {
@@ -64,4 +105,43 @@ func (c *CharacterClient) BuyBankExpansion() (*BankExpansionData, Error) {
 		return nil, err
 	}
 	return &data.Data, nil
+}
+
+func (c *MyAccountClient) buildError(resp *http.Response) Error {
+	switch resp.StatusCode {
+	case 200:
+		return nil
+	case 403:
+		return NewForbiddenException()
+	case 404:
+		return NewMapNotFoundException()
+	case 422:
+		return NewUnprocessableEntityException()
+	case 478:
+		return NewNotEnoughResourcesException()
+	case 482:
+		return NewNoItemAtThisPrice()
+	case 483:
+		return NewTransactionAlreadyInProgressException()
+	case 486:
+		return NewActionAlreadyInProgressException()
+	case 487:
+		return NewCharacterHasNoTask()
+	case 488:
+		return NewTaskNotCompletedException()
+	case 490:
+		return NewCharacterAlreadyAtDestinationException()
+	case 493:
+		return NewSkillLevelToLow()
+	case 497:
+		return NewCharacterInventoryFullException()
+	case 498:
+		return NewCharacterNotFoundException()
+	case 499:
+		return NewInCoolDownException()
+	case 598:
+		return NewResourceNotFoundException()
+	default:
+		return nil
+	}
 }
